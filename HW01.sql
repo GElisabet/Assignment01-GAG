9892063164a46c42f11bb13b0f2593b181129b7b
GElisabet <gudbjorges21@ru.is>
2023-01-23 17:17:25 +0000

    Update HW01.sql

--- HW01.sql
+++ HW01.sql
@@ -1,30 +1,31 @@
--- bla ble blú 
+
+--A.
+select *
+from condition
 
 
---1
 
 select COUNT (*)
-from condition 
-where name like '%injur%'
+from condition
+where name like '%kidney%' or name like '%Kidney%'
 
---EKKI RETT-- 
 
---2.  The average salary of all registered nurses is 77265 (rounded). What is the average salary of all registered technicians (rounded)?
+--B.  The average salary of all registered nurses is 77265 (rounded). What is the average salary of all registered technicians (rounded)?
 
 select AVG(ROUND(salary,2)) AS AverageSalary
 from HealthcareWorker,role
 where HealthcareWorker.ID = role.ID
-and role.name = 'Technician'
+and role.name = 'Technician' 
 
 select AVG(ROUND(salary,2)) AS AverageSalary
 from HealthcareWorker
 join role on HealthcareWorker.ID = role.ID
-and role.name = 'Nurse'
+and role.name = 'Nurse' 
 
 --EKKI RETT-- 
 
 
---3  There were 5510 admissions to private hospitals. How many admissions were there to government hospitals?
+--C. There were 5510 admissions to private hospitals. How many admissions were there to government hospitals?
 
 select COUNT(*) AS TotalAdmissions
 from Admitted,hospital
@@ -33,17 +34,13 @@ and hospital.type = 'Government'
 
 -- Rétt -- 
 
--- 4.  Three healthcare workers have quit more than once. How many healthcare workers have quit at least once?
+-- D.  Three healthcare workers have quit more than once. How many healthcare workers have quit at least once?
 
-select COUNT(*) 
-from works
+select COUNT(*) from works
 where quit_date is not null
-and quit_date > start_date 
 
 
--- EKKI RETT --
-
--- 5.  How many patients have been admitted to a hospital in the same city as they live in?
+-- E.  How many patients have been admitted to a hospital in the same city as they live in?
 
 select COUNT(*)
 from Admitted,patient,hospital
@@ -53,9 +50,17 @@ and patient.city = hospital.city
 
 -- veit ekki með þetta -- 
 
--- 6. There were 173 patients admitted to a hospital more than 2 times. How many patients were admitted more than 3 times?
+-- F. There were 173 patients admitted to a hospital more than 2 times. How many patients were admitted more than 3 times?
 
 
+select SUM(ID)
+from patient
+join admitted on patient.ID = admitted.PID
+group by patient.ID
+having COUNT(ID) > 3
 
 
+-- G. For 119 nurses there exist another nurse with the same name. For how many physicians does there exist another physician with the same name?
 
+select COUNT(*)
+from HealthcareWorker
